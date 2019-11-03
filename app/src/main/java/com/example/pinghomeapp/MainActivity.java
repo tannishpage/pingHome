6d437c852495af6a86f63f647a187b1286730703
tannishpage <tannishpage1@gmail.com>
2019-11-03 21:59:36 +1000

    networking is working now, sending and receiving messages is functional

--- app/src/main/java/com/example/pinghomeapp/MainActivity.java
+++ app/src/main/java/com/example/pinghomeapp/MainActivity.java
@@ -4,58 +4,109 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.net.*;
 import java.io.*;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView textEdit;
+    public Thread connThread = new Thread();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button conBtn = (Button) findViewById(R.id.conBtn);
+        Button disBtn = (Button) findViewById(R.id.disBtn);
+        textEdit = (TextView) findViewById(R.id.connStatText);
         conBtn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
                 TextView connStat = (TextView) findViewById(R.id.connStatText);
-                boolean connection = connect("192.168.43.81", 5000);
-                if (connection)
-                {
-                    connStat.setText("Connected");
-                }else
-                    {
-                        connStat.setText("Disconnected");
-                    }
+                if (!(connThread.isAlive())){
+                    connThread = testConnect("192.168.0.37", 5000);
+                }
             }
         });
 
+        disBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                textEdit.setText("Disconnected");
+                connThread.interrupt();
+            }
+        });
     }
 
-    private boolean connect(String ipaddress, int port)
+    public Thread testConnect(final String ipaddress, final int port)
     {
-        try
-        {
-            Socket client = new Socket(ipaddress, port);
-            OutputStream outToServer = client.getOutputStream();
-            DataOutputStream out = new DataOutputStream(outToServer);
-            InputStream inFromServer = client.getInputStream();
-            DataInputStream in = new DataInputStream(inFromServer);
+        final Handler handler = new Handler();
+        boolean status;
+        Thread thread = new Thread(new Runnable() {
 
-            out.writeUTF("PING");
-            if (in.readUTF().compareTo("PONG") == 0)
-            {
-                return true;
-            } else
-                {
-                    return false;
+            @Override
+            public void run(){
+                int retryCount = 0;
+                while (true){
+
+                    try
+                    {
+                        Socket client = new Socket(ipaddress, port);
+                        OutputStream outToServer = client.getOutputStream();
+                        DataOutputStream out = new DataOutputStream(outToServer);
+                        BufferedReader input = new BufferedReader(
+                                new InputStreamReader(client.getInputStream()));
+
+
+                        out.writeUTF("PING");
+                        if (input.readLine().compareTo("PONG") == 0)
+                        {
+                            retryCount = 0;
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textEdit.setText("Connected");
+                                }
+                            });
+                            System.out.println("Yay, it works");
+                        } else
+                        {
+                            System.out.println("No, it doesn't work");
+                        }
+                    } catch(IOException e)
+                    {
+                        retryCount += 1;
+                        if (retryCount >= 100){
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textEdit.setText("Connection Timed Out");
+                                }
+                            });
+                            break;
+                        }
+                        e.printStackTrace();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                textEdit.setText("Retrying...");
+                            }
+                        });
+                    }
+                    try{
+                    Thread.sleep(10000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                        break;
+                    }
                 }
-        } catch(IOException e)
-        {
-            e.printStackTrace();
-            return false;
-        }
+            }
+        });
 
+        thread.start();
+
+        return thread;
     }
 }
